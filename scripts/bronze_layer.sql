202d60757664abc48135add640668041f2f05611
jeffenon <jeff.enon@gmail.com>
2026-06-11 22:28:56 +0800

    Update bronze_layer.sql

--- scripts/bronze_layer.sql
+++ scripts/bronze_layer.sql
@@ -52,3 +52,34 @@ CREATE OR REPLACE TABLE erp_product_categories (
     sub_category VARCHAR(50),
     maintenance VARCHAR(50)
 ) USING DELTA;
+
+-- Insert data into the tables
+COPY INTO crm_customers
+FROM '/Volumes/data_warehouse/default/datasets/crm/customers.csv'
+FILEFORMAT = CSV
+FORMAT_OPTIONS ('header'='true', 'inferSchema'='true');
+
+COPY INTO crm_orders
+FROM '/Volumes/data_warehouse/default/datasets/crm/sales.csv'
+FILEFORMAT = CSV
+FORMAT_OPTIONS ('header'='true', 'inferSchema'='true');
+
+COPY INTO crm_products
+FROM '/Volumes/data_warehouse/default/datasets/crm/products.csv'
+FILEFORMAT = CSV
+FORMAT_OPTIONS ('header'='true', 'inferSchema'='true');
+
+COPY INTO erp_customers
+FROM '/Volumes/data_warehouse/default/datasets/erp/customers.csv'
+FILEFORMAT = CSV
+FORMAT_OPTIONS ('header'='true', 'inferSchema'='true');
+
+COPY INTO erp_locations
+FROM '/Volumes/data_warehouse/default/datasets/erp/locations.csv'
+FILEFORMAT = CSV
+FORMAT_OPTIONS ('header'='true', 'inferSchema'='true');
+
+COPY INTO erp_product_categories
+FROM '/Volumes/data_warehouse/default/datasets/erp/product_categories.csv'
+FILEFORMAT = CSV
+FORMAT_OPTIONS ('header'='true', 'inferSchema'='true');
